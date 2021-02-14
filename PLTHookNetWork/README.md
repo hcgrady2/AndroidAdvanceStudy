d77f216fd7aec22301257284c9ecf0834d70a67b
hcc007 <hcwang1024@163.com>
2021-02-14 14:22:59 +0800

    plt hoot network

---
+++ PLTHookNetWork/README.md
@@ -0,0 +1,61 @@
+# Chapter17
+
+该项目展示了如何使用 PLT Hook 技术来获取网络请求相关信息。
+
+运行环境
+=====
+AndroidStudio3.2
+NDK16~19
+支持 `x86` `armeabi-v7a`
+
+说明
+====
+
+运行项目后点击`开启 Socket Hook`按钮，然后点击`请求`按钮。
+
+整个实现与[ThreadHook](https://github.com/AndroidAdvanceWithGeektime/Chapter06-plus)基本一样，大家可以参考之前的文档
+
+实现差异
+====
+不过跟ThreadHook相比，整体实现还有略有差异，这一次我们增加了一个`hook_plt_method_all_lib`方法
+
+```
+int hook_plt_method_all_lib(const char* exclueLibname, const char* name, hook_func hook) {
+  if (refresh_shared_libs()) {
+    // Could not properly refresh the cache of shared library data
+    return -1;
+  }
+
+  int failures = 0;
+
+  for (auto const& lib : allSharedLibs()) {
+      if (strcmp(lib.first.c_str(), exclueLibname) != 0) {
+        failures += hook_plt_method(lib.first.c_str(), name, hook);
+      }
+  }
+
+  return failures;
+}
+```
+
+它的作用是不仅仅hook某个so，而是hook内存中的所有so。但是要排除掉方法本身定义的so，不然运行期间会出问题。
+
+
+我们可以通过sa_family来判断类型，需要过滤掉local socket
+
+```
+if (serv_addr->sa_family == AF_INET) {
+   // ipv4地址
+
+} else if(serv_addr->sa_family == AF_INET6) {
+   // ipv6地址
+
+} else if (serv_addr->sa_family == AF_LOCAL) {
+   // local socket
+}
+```
+
+通过connect函数的hook，我们可以做的东西很多，例如：
+  1. 禁用应用网络访问
+  2. 过滤广告ip
+  3. 禁用定位功能
---
+++ PLTHookNetWork/SocketHookSample/src/main/cpp/socketHook.cpp
@@ -0,0 +1,175 @@
+#include <jni.h>
+#include <string>
+
+#include <atomic>
+#include <dlfcn.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sstream>
+#include <android/log.h>
+#include <unordered_set>
+#include <fcntl.h>
+#include <sys/fcntl.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <syscall.h>
+#include <sys/socket.h>
+#include <linux/in.h>
+#include <endian.h>
+#include <arpa/inet.h>
+#include "linker.h"
+#include "hooks.h"
+
+#define  LOG_TAG    "HOOOOOOOOK"
+#define  ALOG(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
+std::atomic<bool> socket_hooked;
+
+static jclass kJavaClass;
+static jmethodID kMethodGetStack;
+static JavaVM *kJvm;
+
+
+char *jstringToChars(JNIEnv *env, jstring jstr) {
+    if (jstr == nullptr) {
+        return nullptr;
+    }
+
+    jboolean isCopy = JNI_FALSE;
+    const char *str = env->GetStringUTFChars(jstr, &isCopy);
+    char *ret = strdup(str);
+    env->ReleaseStringUTFChars(jstr, str);
+    return ret;
+}
+
+void printJavaStack() {
+    JNIEnv* jniEnv = NULL;
+    // JNIEnv 是绑定线程的，所以这里要重新取
+    kJvm->GetEnv((void**)&jniEnv, JNI_VERSION_1_6);
+    jstring java_stack = static_cast<jstring>(jniEnv->CallStaticObjectMethod(kJavaClass, kMethodGetStack));
+    if (NULL == java_stack) {
+        return;
+    }
+    char* stack = jstringToChars(jniEnv, java_stack);
+    ALOG("stack:%s", stack);
+    free(stack);
+
+    jniEnv->DeleteLocalRef(java_stack);
+}
+
+
+int socket_connect_hook(int sockfd, const struct sockaddr* serv_addr, socklen_t addrlen) {
+    ALOG("socket_connect_hook sa_family: %d", serv_addr->sa_family);
+
+    char ip[128]={0};
+    int port=-1;
+    if (serv_addr->sa_family == AF_INET) {
+        printJavaStack();
+        struct sockaddr_in *sa4=(struct sockaddr_in*)serv_addr;
+        inet_ntop(AF_INET,(void*)(struct sockaddr*)&sa4->sin_addr,ip,128);
+        port=ntohs(sa4->sin_port);
+        ALOG("AF_INET ipv4 connect IP===>%s:%d",ip,port);
+    } else if(serv_addr->sa_family == AF_INET6) {
+        printJavaStack();
+
+        struct sockaddr_in6 *sa6 = (struct sockaddr_in6 *) serv_addr;
+        char *ipv6 = NULL;
+        inet_ntop(AF_INET6, (void *) (struct sockaddr *) &sa6->sin6_addr, ip, 128);
+        ipv6 = strstr(ip, "f:");
+        if (NULL != ipv6) {
+            strcpy(ip, ipv6 + 2);
+        }
+        port = ntohs(sa6->sin6_port);
+        ALOG("AF_INET6 ipv6 IP===>%s:%d", ip, port);
+    } else if (serv_addr->sa_family == AF_LOCAL) {
+        ALOG("Ignore local socket connect");
+
+    }
+
+    return CALL_PREV(socket_connect_hook, sockfd, serv_addr, addrlen);
+}
+
+ssize_t socket_send_hook(int sockfd, const void *buf, size_t len, int flags) {
+    return CALL_PREV(socket_send_hook, sockfd, buf, len, flags);
+}
+
+ssize_t socket_recv_hook(int sockfd, void *buf, size_t len, int flags) {
+    return CALL_PREV(socket_recv_hook, sockfd, buf, len, flags);
+}
+
+ssize_t socket_sendto_hook(int sockfd, const void *buf, size_t len, int flags,
+                           const struct sockaddr *dest_addr, socklen_t addrlen) {
+    return CALL_PREV(socket_sendto_hook, sockfd, buf, len, flags, dest_addr, addrlen);
+}
+
+ssize_t socket_recvfrom_hook(int sockfd, void *buf, size_t len, int flags,
+                             struct sockaddr *src_addr, socklen_t *addrlen) {
+    return CALL_PREV(socket_recvfrom_hook, sockfd, buf, len, flags, src_addr, addrlen);
+}
+
+/**
+* 直接 hook 内存中的所有so，但是需要排除掉socket相关方法本身定义的libc(不然会出现循坏)
+* plt hook
+*/
+void hookLoadedLibs() {
+    ALOG("hook_plt_method");
+    hook_plt_method_all_lib("libc.so", "send", (hook_func) &socket_send_hook);
+    hook_plt_method_all_lib("libc.so", "recv", (hook_func) &socket_recv_hook);
+    hook_plt_method_all_lib("libc.so", "sendto", (hook_func) &socket_sendto_hook);
+    hook_plt_method_all_lib("libc.so", "recvfrom", (hook_func) &socket_recvfrom_hook);
+    hook_plt_method_all_lib("libc.so", "connect", (hook_func) &socket_connect_hook);
+}
+
+
+void enableSocketHook() {
+    if (socket_hooked) {
+        return;
+    }
+    ALOG("enableSocketHook");
+
+    socket_hooked = true;
+    if (linker_initialize()) {
+        throw std::runtime_error("Could not initialize linker library");
+    }
+    hookLoadedLibs();
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_dodola_socket_SocketHook_enableSocketHookNative(JNIEnv *env, jclass type) {
+
+    enableSocketHook();
+}
+
+static bool InitJniEnv(JavaVM *vm) {
+    kJvm = vm;
+    JNIEnv* env = NULL;
+    if (kJvm->GetEnv((void**)&env, JNI_VERSION_1_6) != JNI_OK){
+        ALOG("InitJniEnv GetEnv !JNI_OK");
+        return false;
+    }
+    kJavaClass = reinterpret_cast<jclass>(env->NewGlobalRef(env->FindClass("com/dodola/socket/SocketHook")));
+    if (kJavaClass == NULL)  {
+        ALOG("InitJniEnv kJavaClass NULL");
+        return false;
+    }
+
+    kMethodGetStack = env->GetStaticMethodID(kJavaClass, "getStack", "()Ljava/lang/String;");
+    if (kMethodGetStack == NULL) {
+        ALOG("InitJniEnv kMethodGetStack NULL");
+        return false;
+    }
+    return true;
+}
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved){
+    ALOG("JNI_OnLoad");
+
+
+    if (!InitJniEnv(vm)) {
+        return -1;
+    }
+
+    return JNI_VERSION_1_6;
+}
+
